1163786065d7e109ed6ab38983570a3dc8e8e68c
ayushdwivedi1280 <ayushdwivedi3008@gmail.com>
2025-11-03 00:38:55 +0530

    added base in viti.config.js
    to solve problem of making it live using GitHub page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "AnimeChat_UI_2.0",
   optimizeDeps: {
     exclude: ['lucide-react']
   }
